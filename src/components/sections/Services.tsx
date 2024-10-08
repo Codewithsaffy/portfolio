88d2b1e000acb30d47373378b564dc00e4bfe4d5
codewithsaffy <codewithsaffy@gmail.com>
2024-10-08 05:03:14 +0500

    add

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -8,7 +8,7 @@ const Services = () => {
       id="services"
       className="z-10 w-full min-h-screen flex-col flex  items-center gap-10 md:gap-20 mx-auto px-4  md:px-8 lg:px-16 py-16 bg-[#000000]  "
     >
-      <h2 className="md:text-4xl text-4xl  z-10 text-center font-bold">
+      <h2 className="text-3xl sm:text-4xl font-bold z-10 text-center">
         My <span className="text-[#CBACF9]">Services</span>
       </h2>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 w-full z-10 ">
--- src/components/sections/Testinominal.tsx
+++ src/components/sections/Testinominal.tsx
@@ -5,7 +5,7 @@ import testimonials from "@/data/testinominal";
 const Testinominal = () => {
   return (
     <section id="testimonial" className="z-10 w-full flex-col flex  items-center gap-10 md:gap-20 mx-auto px-4  md:px-8 lg:px-16 py-16">
-      <h2 className="text-4xl text-center z-10 font-bold">
+      <h2 className="text-3xl sm:text-4xl font-bold z-10 text-center">
         Kind words from <span className="text-[#CBACF0]">satisfied clients</span>
       </h2>
       <InfiniteMovingCards
