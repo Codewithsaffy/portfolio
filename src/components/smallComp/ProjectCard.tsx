23a20082e83a0a9af4c553ce911d3f15aaa37141
codewithsaffy <codewithsaffy@gmail.com>
2024-10-08 05:10:58 +0500

    add all

--- src/components/smallComp/ProjectCard.tsx
+++ src/components/smallComp/ProjectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard = ({
 }) => {
   return (
     <div
-      className={`w-full sm:w-[350px] p-4 rounded-lg border border-dark-accent/30 z-10 bg-gradient-to-r from-[#04071D] to-[#0C0E23] flex flex-col gap-4 ${className}`}
+      className={`w-full sm:w-[350px] p-4 rounded-lg border border-dark-accent/30 z-10 bg-gradient-to-r from-[#04071D] to-[#0C0E23] flex flex-col gap-4 ${className} transition-all duration-300 ease-in-out transform hover:scale-105`}
     >
       <div className="bg-gradient-to-r h-[200px] rounded-lg overflow-hidden from-[#13162D] to-[#404793] relative">
         <Image
@@ -27,30 +27,30 @@ const ProjectCard = ({
         />
       </div>
 
-      <div className="flex flex-col gap-4 w-full">
+      <div className="flex flex-col gap-2 w-full">
         <h2 className="text-2xl font-bold">{data.name}</h2>
         <p className="text-sm text-gray-300">{data.description}</p>
 
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mt-2">
           <div className="flex items-center">
             {data.icons.map((icon, i) => (
               <div
                 key={i}
-                className="rounded-full bg-gray-800/30 w-8 h-8 flex justify-center items-center opacity-100" 
+                className="rounded-full bg-gray-800/30 w-8 h-8 flex justify-center items-center transition-all duration-300 ease-in-out transform hover:scale-105"
               >
-                <Image  src={icon} alt="icon" width={18} height={18} />
+                <Image src={icon} alt="icon" width={18} height={18} />
               </div>
             ))}
           </div>
 
           <div className="flex justify-end items-center gap-2">
-            <div className="bg-green-500 p-2 rounded-full hover:scale-105 transition-transform duration-200">
+            <div className="bg-green-500 p-2 rounded-full transition-transform duration-300 ease-in-out transform hover:scale-110">
               <Link href={data.gitHub} className="hover:cursor-pointer">
                 <FaCode className="text-white" />
               </Link>
             </div>
 
-            <div className="p-2 rounded-full flex justify-center items-center gap-1 text-blue-500 hover:scale-105 transition-transform duration-200">
+            <div className="p-2 rounded-full flex justify-center items-center gap-1 text-blue-500 transition-transform duration-300 ease-in-out transform hover:scale-110">
               <Link
                 href={data.link}
                 className="hover:cursor-pointer flex gap-1 justify-center items-center"
