0d2880bac3c26d9bfca05aa8e149816f890b93ab
Muhammad Yousuf <codewithsaffy@gmail.com>
2025-06-01 01:58:15 +0500

    fix bug

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 const LoadingPage = () => {
   const [isLoaded, setIsLoaded] = useState(false);
